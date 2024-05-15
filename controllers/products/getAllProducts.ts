730d92d4403fb2d778d652a7321d7976229c8b99
Arsen Hryhoriak <arsen.hryh@gmail.com>
2024-05-15 13:30:06 +0300

    refactor(getAllproducts) pagination

--- controllers/products/getAllProducts.ts
+++ controllers/products/getAllProducts.ts
@@ -3,16 +3,31 @@ import Product from '../../models/Product';
 import ctrlWrapper from '../../utils/ctrlWrapper';
 
 const getAllProducts = ctrlWrapper(async (req: Request, res: Response) => {
-  const { q = '' } = req.query as { q?: string };
+  const {
+    q = '',
+    page = '1',
+    limit = '10',
+  } = req.query as { q?: string; page?: string; limit?: string };
 
   const products = await Product.find({
     $or: [
       { name: { $regex: q, $options: 'i' } },
       { article: { $regex: q, $options: 'i' } },
     ],
+  })
+    .skip((+page - 1) * +limit)
+    .limit(+limit);
+
+  const total = await Product.countDocuments({
+    $or: [
+      { name: { $regex: q, $options: 'i' } },
+      { article: { $regex: q, $options: 'i' } },
+    ],
   });
-  res.json(products);
+
+  const totalPages = Math.ceil(total / +limit);
+
+  res.json({ products, total, totalPages });
 });
-// TODO: Add pagination
 
 export default getAllProducts;
