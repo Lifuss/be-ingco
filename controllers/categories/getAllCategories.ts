1006d372bcf82c35b193b1be0ab9ee2d3ca5374f
Arsen Hryhoriak <arsen.hryh@gmail.com>
2024-05-24 18:28:42 +0300

    refactor(getCagetoties) response count for stats in dashboard

--- controllers/categories/getAllCategories.ts
+++ controllers/categories/getAllCategories.ts
@@ -1,16 +1,30 @@
 import { Request, Response } from 'express';
 import Category from '../../models/Category';
 import ctrlWrapper from '../../utils/ctrlWrapper';
+import Product from '../../models/Product';
 
 const getAllCategories = ctrlWrapper(async (req: Request, res: Response) => {
-  const { q = '' } = req.query as { q?: string };
+  const { q = '' } = req.query as {
+    q?: string;
+  };
+
   const categories = await Category.find(
     {
       name: { $regex: q, $options: 'i' },
     },
     'name _id',
   );
-  res.json(categories);
+
+  const countProductWithinCategory = await Promise.all(
+    categories.map(async (categoryItem) => {
+      const count = await Product.countDocuments({
+        category: categoryItem._id,
+      });
+      return { ...categoryItem.toObject(), count };
+    }),
+  );
+
+  res.json(countProductWithinCategory);
 });
 
 export default getAllCategories;
