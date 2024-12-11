cb12528d6902f9c44c27d98ac880f5ef8f4b659b
Arsen Hryhoriak <arsen.hryh@gmail.com>
2024-12-11 17:43:32 +0200

    logger update and add errors logging

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
@@ -14,28 +15,11 @@ import statsRouter from './routes/stats';
 dotenv.config();
 
 const app = express();
-// TODO: update logger to min useful information
-// Logger for static files
-const staticFilesLogger = morgan(function (tokens, req, res) {
-  let url = tokens.url(req, res);
-  if (url && url.startsWith('/static')) {
-    url = url.split('_').pop();
-  }
-  return [
-    tokens.method(req, res),
-    url,
-    tokens.status(req, res),
-    '-',
-    tokens['response-time'](req, res),
-    'ms',
-  ].join(' ');
-});
-
-const format = process.env.NODE_ENV === 'production' ? 'combined' : 'dev';
 
 app.use(cors());
 
-app.use('/static', staticFilesLogger);
+// static files
+app.use('/static', morgan(':status :response-time ms :url'));
 app.use(
   '/static',
   express.static(path.resolve('static'), {
@@ -47,22 +31,41 @@ app.use(
 
 app.use(express.json());
 
-app.use(morgan(format));
+const loggerType =
+  process.env.NODE_ENV === 'development'
+    ? 'dev'
+    : ':method :date[clf] :url :status - :response-time ms';
+
+app.use(morgan(loggerType));
+
+// CRUD handlers
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
 app.use('/api/categories', categoryRouter);
 app.use('/api/orders', orderRouter);
 app.use('/api/stats', statsRouter);
 
+// 404 handler
 app.use((req: Request, res: Response) => {
   const error = new Error('Not Found - ' + req.originalUrl);
   res.status(404).json({ message: error.message });
 });
 
+// global error handler
+// 4 params is needed as error sign for middleware error handler
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((err: RequestError, req: Request, res: Response, _: NextFunction) => {
   const { status = 500, message = 'Server error' } = err;
-  console.log(err.message);
+  const logMessage = `[${new Date().toISOString()}] [${status}] ${message} - ${req.method} ${req.originalUrl}\n`;
+
+  const logFilePath = path.join(__dirname, 'errors.log');
+  fs.appendFile(logFilePath, logMessage, 'utf8', (err) => {
+    if (err) {
+      console.error('Failed to write to log file:', err);
+    }
+  });
+  console.error(logMessage);
+
   res.status(status).json({ message });
 });
 
